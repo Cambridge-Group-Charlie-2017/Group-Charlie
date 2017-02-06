31d498bd5f0dd11b9a9771dc062760c3e911ccd2
M Boyce <boycem97@hotmail.com>
2017-02-06 16:13:36 +0000

    Clusterer: Added sender based cluster naming

---
+++ src/main/java/uk/ac/cam/cl/charlie/clustering/ClusterNamingException.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cl.charlie.clustering;
+
+/**
+ * @author M Boyce
+ */
+public class ClusterNamingException extends Exception{
+    public ClusterNamingException(String msg){
+        super(msg);
+    }
+}
