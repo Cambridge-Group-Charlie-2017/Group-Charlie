264b7382c58ad2b28b5c0f7a04337bfa4281c5c4
Shyam Tailor <shyamatailor@gmail.com>
2017-02-07 12:11:27 +0000

    vectoriser: unchecked build, adding sql

--- src/main/java/uk/ac/cam/cl/charlie/vec/tfidf/Tfidf.java
+++ src/main/java/uk/ac/cam/cl/charlie/vec/tfidf/Tfidf.java
@@ -7,17 +7,21 @@ import java.util.TreeSet;
 import uk.ac.cam.cl.charlie.db.Database;
 import uk.ac.cam.cl.charlie.vec.Document;
 
-import javax.print.Doc;
-
 /**
  * Created by shyam on 05/02/2017. Edited by LP 05/02/2017
  */
 public final class Tfidf {
     // Presumably this will have to be stored in the database (which I will leave for later).
     // Use singleton pattern to stop subclassing
+    private static String INSERT_SQL = "INSERT INTO WORD_FREQUENCIES VALUES (?, ?)";
+    private static String TOTAL_NUMBER_OF_DOCS = "";
+
+    private static String GET_SQL = "SELECT freq FROM WORD_FREQUENCIES WHERE word = ?";
 
     private static Tfidf instance = null;
     private Database database; // the database instance should store the word frequencies.
+    private PreparedStatement insertStmt;
+    private PreparedStatement getStmt;
 
     // TODO note I strongly disagree with this class having:
     // a) any tables (the data has to be persisted to a database
@@ -31,24 +35,19 @@ public final class Tfidf {
     private Tfidf() throws SQLException {
         database = Database.getInstance();
 
-        if (database.tableExists("WORD_FREQUENCIES")) {
-            // there exists a table in this database with word frequencies
-            return;
-        }
+        insertStmt = database.getConnection().prepareStatement(INSERT_SQL);
 
-        else {
+        getStmt = database.getConnection().prepareStatement(GET_SQL);
+
+        if (!database.tableExists("WORD_FREQUENCIES")) {
             // need to create the database table
-            String sql = "CREATE TABLE WORD_FREQUENCIES(word VARCHAR(50) NOT NULL,count INTEGER NOT NULL)";
-            Statement stmt = conn.createStatement();
-            stmt.execute(sql);
+            String sql = "CREATE TABLE WORD_FREQUENCIES(word VARCHAR(50) NOT NULL,freq INTEGER NOT NULL)";
+            database.executeUpdate(sql);
 
             // insert a value to represent the number of documents in total:
-            sql = "INSERT INTO WORD_FREQUENCIES(word,count) VALUES (totalNumberOfDocs,0)";
-            stmt.execute(sql);
-
-            stmt.close();
-            conn.commit();
-            conn.close();
+            stmt.setString(1, TOTAL_NUMBER_OF_DOCS);
+            stmt.setInt(2, 0);
+            stmt.executeUpdate(sql);
         }
     }
 
@@ -59,16 +58,37 @@ public final class Tfidf {
         return instance;
     }
 
-    public int totalNumberDocuments() {
-        return 0;
+    public int totalNumberDocuments() throws SQLException {
+        getStmt.setString(1, TOTAL_NUMBER_OF_DOCS);
+        ResultSet rs = getStmt.executeQuery();
+        if (rs.next()) {
+            return rs.getInt(1); // check this line
+        }
+
+        else {
+            throw new SQLException();
+        }
     }
     
     public int numberOfDocsWithWith(String word) {
-        return 0;
+        getStmt.setString(1, word);
+        ResultSet rs = getStmt.executeQuery();
+        if (rs.next()) {
+            return rs.getInt(1); // check this line
+        }
+
+        else {
+            throw new SQLException();
+        }
+    }
+
+    public void addCountToWord(String word) {
+        
     }
     
     public void addDocument(Document doc) {
-        // TODO
+        // Generate a hashmap with all keys in lowercase, then update database
+
     }
     
     public void addDocument(Document doc, HashMap<String, Integer> wordfrequency) {
