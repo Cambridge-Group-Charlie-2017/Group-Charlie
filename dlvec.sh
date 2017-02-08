6e61b85ada4ebbc72f4d718c547919294a7675b6
Shyam Tailor <shyamatailor@gmail.com>
2017-02-08 14:34:50 +0000

    build: swapped from bash script to gradle task for download (cross platform

--- dlvec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ ! -f src/main/resources/word2vec/wordvectors.txt ]; then
-    echo "word2vec not found; downloading now"; mkdir -p src/main/resources/word2vec; curl -L -o src/main/resources/word2vec/wordvectors.txt \
-                            https://www.dropbox.com/s/8q626nhgrx6ez5m/wordvectors.txt?dl=1
-fi
