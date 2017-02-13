23ac8bffb6d1ea9aca9e90ba5a61c4fbf96f19fd
M Boyce <boycem97@hotmail.com>
2017-02-13 20:24:53 +0000

    Clustering: Fixing ClusterNamerTests

--- src/test/java/uk/ac/cam/cl/charlie/clustering/ClusterNamerTest.java
+++ src/test/java/uk/ac/cam/cl/charlie/clustering/ClusterNamerTest.java
@@ -14,9 +14,10 @@ public class ClusterNamerTest {
     @Test public void senderNameTest1() throws Exception{
         ArrayList<ClusterableObject> messages= new ArrayList<ClusterableObject>();
         ArrayList<File> files = new ArrayList<File>();
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 250; i++)
             messages.add(new ClusterableMessage(MessageCreator.createMessage(
-               "blabla@gmail.com","Bob@companyname.com","Project X", "",files)));
+               "blabla@gmail.com","Bob@companyname.com","Project X", "A",files)));
+        GenericDummyVectoriser.train(messages);
 
         GenericEMCluster c = new GenericEMCluster(messages);
         ClusterNamer.senderNaming(c);
@@ -26,9 +27,11 @@ public class ClusterNamerTest {
     @Test public void senderNameTest2(){
         ArrayList<ClusterableObject> messages= new ArrayList<ClusterableObject>();
         ArrayList<File> files = new ArrayList<File>();
-        for (char letter = 'a'; letter <= 'j'; letter++)
+        for (int i = 0; i < 250; i++)
             messages.add(new ClusterableMessage(MessageCreator.createMessage(
-               "blabla@gmail.com","Bob@"+letter+"-companyname.com","Project X", "",files)));
+               "blabla@gmail.com","Bob@"+i+"-companyname.com","Project X", "",files)));
+
+        GenericDummyVectoriser.train(messages);
 
         GenericEMCluster c = new GenericEMCluster(messages);
 
@@ -69,16 +72,19 @@ public class ClusterNamerTest {
     @Test public void nameTest(){
         ArrayList<ClusterableObject> messages= new ArrayList<ClusterableObject>();
         ArrayList<File> files = new ArrayList<File>();
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@a-companyname.com","Project X", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@b-companyname.com","Project X another", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@c-companyname.com","Project X request", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@d-companyname.com","Project X", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@e-companyname.com","Project X blaa", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@f-companyname.com","Project X ", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@g-companyname.com","Project X", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@h-companyname.com","Project X", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@i-companyname.com","Project X", "",files)));
-        messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com","Bob@j-companyname.com","Project X", "",files)));
+        for(int i=0; i<25;i++) {
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@a-companyname.com", "Project X", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@b-companyname.com", "Project X another", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@c-companyname.com", "Project X request", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@d-companyname.com", "Project X", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@e-companyname.com", "Project X blaa", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@f-companyname.com", "Project X ", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@g-companyname.com", "Project X", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@h-companyname.com", "Project X", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@i-companyname.com", "Project X", "A", files)));
+            messages.add(new ClusterableMessage(MessageCreator.createMessage("blabla@gmail.com", "Bob@j-companyname.com", "Project X", "A", files)));
+        }
+        GenericDummyVectoriser.train(messages);
 
         GenericEMCluster c = new GenericEMCluster(messages);
 
